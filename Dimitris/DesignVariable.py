2e2a7df9b3a396f2d11baf7334e295b965368687
dimitrisdimos00 <56233132+dimitrisdimos00@users.noreply.github.com>
2021-05-12 15:21:38 +0300

    before chanching designVec to having initialize function

--- Dimitris/DesignVariable.py
+++ Dimitris/DesignVariable.py
@@ -8,29 +8,9 @@ class designVec:
         
         self.q = int(np.ceil( np.log2( (maxValue - minValue) / dx + 1 ) ))
 
-        # self.case = self.findCase(self.min, self.max)
         self.value_bin = self.generateBinary(self.q, self.min, self.max)
         self.value_dec = self.convertToDecimal(self.q, self.min, self.max, self.value_bin)
         
-
-    def findCase(self, min, max):
-        case = 1
-        if min < 0 and max > 0:
-            if max > abs(min):
-                case = 1 
-            elif max < abs(min):
-                case = 2 
-            else:
-                # equal
-                case = 3 
-        elif min < 0 and max <= 0:
-            # case = 4 => both min and max are negative
-            case = 4 
-        else:
-            # case = 5 => both min and max are positive
-            case = 5 
-        return case
-
 
     def generateBinary(self, q, min, max):
         
@@ -41,32 +21,11 @@ class designVec:
 
         dec = self.convertToDecimal(q, min, max, bin_array)
 
-        # compute the sign according to the case of min and max parameters
-        # sign = 0 # plus
-        # if case == 1:
-        #     if dec <= abs(min):
-        #         sign = random.randint(0, 1)
-        #     else:
-        #         sign = 0
-        # elif case == 2:
-        #     if dec <= abs(max):
-        #         sign = random.randint(0, 1)
-        #     else:
-        #         sign = 1
-        # elif case == 3:
-        #     sign = random.randint(0, 1)
-        # elif case == 4:
-        #     sign = 1
-        # else:
-        #     sign = 0
-        
-        # bin_array[0] = sign
-
         return bin_array
 
-    # ignores the first bit(sign)
+
     def convertToDecimal(self, q, min, max, value_bin):
-        
+    
         sum = 0
         i = 0
         for k in range(q - 1, -1, -1):
@@ -74,16 +33,7 @@ class designVec:
             i += 1
         x = min + (max - min) / (2 ** q - 1) * sum
         
-        # sign = value_bin[0]
-        # if sign == 1:
-        #     x = x * (-1)
-        
         return x
-
-    def calculateCrossover(self, dv1, dv2):
-        q = dv1.q
-        site = random.randint(0, q - 1)
-
 
 
     def printDesignVector(self):
--- Dimitris/Operations.py
+++ Dimitris/Operations.py
@@ -7,6 +7,7 @@ def f(x):
 class operations:
     def __init__(self, population):
         self.population = population
+        self.designVectors = self.population.all_design_vectors
         self.n = population.pop_size
 
         self.fitness_values = self.evaluate()
@@ -16,10 +17,9 @@ class operations:
         self.probabilities_of_selection = self.findProbabilitiesOfSelection()
         self.cummulative_probabilities = self.findCummulativeProbabilities()
 
-        self.mates = self.findMates()
+        self.mates, self.matesNo = self.findMates(self.designVectors, self.cummulative_probabilities)
 
-        
-    
+
     def evaluate(self):
         eval_value = []
         for dv in self.population.all_design_vectors:
@@ -32,7 +32,6 @@ class operations:
         for fv in self.fitness_values:
             sum += fv
         return sum
-
 
     def findProbabilitiesOfSelection(self):
         pos = []
@@ -53,34 +52,88 @@ class operations:
             
         return cumProb
         
-    def findMates(self):
-        selectedMates = []
-        for j in range(self.n):
+    def findMates(self, designVectors, cumProb):
+        n = len(designVectors)
+        selectedMates = [None] * n
+        selectedMatesIndex = [None] * n
+
+        for j in range(n):
             r = random.uniform(0,1)
-         
-            length = len(self.cummulative_probabilities)
-            for i in range(1, length):
-                if r > self.cummulative_probabilities[i - 1] and r <= self.cummulative_probabilities[i]:
-                    selectedMates.append(i + 1)
-        return selectedMates
+            selectedIndex = 0
+            # find the index of the element to be selected
+            for i in range(1, n):
+                if r > cumProb[i - 1] and r <= cumProb[i]:
+                    selectedIndex = i
+            
+            selectedMates[j] = designVectors[selectedIndex]
+            selectedMatesIndex[j] = selectedIndex
+
+        return selectedMates, selectedMatesIndex
+
+
+    def singlePointMutation(self, bin_value, max_mutation_prob):
+        pm = random.uniform(0, max_mutation_prob)
+        prob_mutation_sucess = random.uniform(0, 1)
+
+        if prob_mutation_sucess <= pm:
+            q = bin_value.size
+            site = random.randint(0, q)
+            if bin_value[site] == 1:
+                bin_value[site] = 0
+            else:
+                bin_value[site] = 1
+        return bin_value
+
+    def crossover(self, bin_value1, bin_value2):
+        q = bin_value1.size
+        site = random.randint(0, q - 1)
+        offspring1 = np.zeros(q, dtype='int')
+        offspring2 = np.zeros(q, dtype='int')
+        
+        for i in range(q):
+            if i <= site:
+                offspring1[i] = bin_value1[i]
+                offspring2[i] = bin_value2[i] 
+            else:
+                offspring1[i] = bin_value2[i]
+                offspring2[i] = bin_value1[i]
+        
+        return offspring1, offspring2
+
+    def calculateNewPopulation(self, mates): 
+        # mates is a list
+        length = len(mates)
+        new__design_vectors = [None] * length
+        for i in range(0, length, 2):
+            parent1 = mates[i]
+            parent2 = mates[i + 1]
+            offspring1, offspring2 = self.crossover(parent1.value_bin, parent2.value_bin)
+            
+            new_population[i] = offspring1
+            new_population[i + 1] = offspring2
+        
+        return new_population
+
 
     def print(self):
         self.population.printPopulation()
-        print("\n Fitness Values: ")
-        print(self.fitness_values)
+        # print("\n Fitness Values: ")
+        # print(self.fitness_values)
 
         print("\n Probability of selection: ")
         print(self.probabilities_of_selection)
 
-        
-        print("\n Cummulative probabilities: ")
-        print(self.cummulative_probabilities)
+        # print("\n Cummulative probabilities: ")
+        # print(self.cummulative_probabilities)
 
         print("\n Average of fitness values: ")
         print(self.averageOfFvals)
 
         print("\n Selected Mates: ")
-        print(self.mates)
+        n = len(self.mates)
+        for i in range(n):
+            print("no. ", self.matesNo[i])
+            print(self.mates[i].printDesignVector())
 
         # allDesVec = self.population.all_design_vectors
         # print("\n Selected Mates Real Values: ")
